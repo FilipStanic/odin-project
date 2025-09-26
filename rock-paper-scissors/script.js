6fcac5910a95de5246d75e63710cb2253bc655a9
FilipStanic <filipkostanic@gmail.com>
2025-09-26 11:34:36 +0200

    Working RPS

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -18,42 +18,50 @@ function playRound() {
     const human = humanDecision();
     const cpu = cpuDecision();
 
-    console.log("CPU chose " + cpu);
+    console.log("Computer chose: " + cpu)
 
     if (human === "Rock" && cpu === "Rock") {
-        console.log("It's a draw!");
+        return "It's a draw!";
     } else if (human === "Rock" && cpu === "Paper") {
-        console.log("CPU Wins!");
+        return "CPU Wins!";
     } else if (human === "Rock" && cpu === "Scissors") {
-        console.log("Human Wins!");
+        return "Human Wins!";
     } else if (human === "Paper" && cpu === "Rock") {
-        console.log("Human Wins!");
+        return "Human Wins!";
     } else if (human === "Paper" && cpu === "Paper") {
-        console.log("It's a draw!");
+        return "It's a draw!";
     } else if (human === "Paper" && cpu === "Scissors") {
-        console.log("CPU Wins!");
+        return "CPU Wins!";
     } else if (human === "Scissors" && cpu === "Rock") {
-        console.log("CPU Wins!");
+        return "CPU Wins!";
     } else if (human === "Scissors" && cpu === "Paper") {
-        console.log("Human Wins!");
+        return "Human Wins!";
     } else if (human === "Scissors" && cpu === "Scissors") {
-        console.log("It's a draw!");
+        return "It's a draw!";
     } else {
-        console.log("Invalid input, please type Rock, Paper, or Scissors.");
+        return "Invalid input, please type Rock, Paper, or Scissors.";
     }
 }
 
-function playGame(playRound){
-    var score = 0;
+function playGame(){
+    let score = 0;
+    while(true){
+        const result = playRound();
 
-    if("Human Wins!" == 1){
-        console.log("testHUM")
-    } else if ("CPU Wins!") {
-        console.log("CPUTEST");
-    } else {
-        console.log("TEST")
+        if (result === "Human Wins!"){
+            score++
+            console.log("Score: " + score);
+        } else if  (result === "CPU Wins!") {
+            console.log("You Lose! Final Score: " + score);
+            score = 0
+            break;
+        } else if (result === "It's a draw!") {
+            console.log("The score is: " + score);
+        } else {
+            console.log("Invalid input")
+            break;
+        }
     }
 }
 
-playRound()
 playGame()
