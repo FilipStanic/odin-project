83ce2a132d31d79875167a85c49f6f14c4f4e8e5
FilipStanic <filipkostanic@gmail.com>
2025-09-26 09:09:31 +0200

    RPS

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,47 @@
+function cpuDecision() {
+    var rndmNumb = Math.floor(Math.random() * 3) + 1;
+    if (rndmNumb === 1) {
+        return "Rock";
+    } else if (rndmNumb === 2) {
+        return "Paper"; 
+    } else {
+        return "Scissors";
+    }
+}
+
+function humanDecision() {
+    var humanChoice = prompt("Please write: Rock, Paper, Scissors (CASE SENSITIVE)");
+    return humanChoice;
+}
+
+function game() {
+    const human = humanDecision();
+    const cpu = cpuDecision();
+
+    console.log("CPU chose " + cpu);
+
+    if (human === "Rock" && cpu === "Rock") {
+        console.log("It's a draw!");
+    } else if (human === "Rock" && cpu === "Paper") {
+        console.log("CPU Wins!");
+    } else if (human === "Rock" && cpu === "Scissors") {
+        console.log("Human Wins!");
+    } else if (human === "Paper" && cpu === "Rock") {
+        console.log("Human Wins!");
+    } else if (human === "Paper" && cpu === "Paper") {
+        console.log("It's a draw!");
+    } else if (human === "Paper" && cpu === "Scissors") {
+        console.log("CPU Wins!");
+    } else if (human === "Scissors" && cpu === "Rock") {
+        console.log("CPU Wins!");
+    } else if (human === "Scissors" && cpu === "Paper") {
+        console.log("Human Wins!");
+    } else if (human === "Scissors" && cpu === "Scissors") {
+        console.log("It's a draw!");
+    } else {
+        console.log("Invalid input, please type Rock, Paper, or Scissors.");
+    }
+}
+
+game();
+
